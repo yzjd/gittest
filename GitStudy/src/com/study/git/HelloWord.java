fb0a3f04257fd46f245ea66038d35a10d4add24b
yzjd <xgg232@126.com>
2017-04-20 14:24:43 +0800

    clone分支  “分支1”

--- GitStudy/src/com/study/git/HelloWord.java
+++ GitStudy/src/com/study/git/HelloWord.java
@@ -13,7 +13,7 @@ public class HelloWord {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//
+		//clone ∑÷÷ß 1
 		System.out.println("hello this is git!");
 	}
 
