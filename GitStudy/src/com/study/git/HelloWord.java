aa886454c4574abd5cb81fb58e411a291450df01
yzjd <xgg232@126.com>
2017-04-20 12:57:58 +0800

    GitStudy first commit

---
+++ GitStudy/src/com/study/git/HelloWord.java
@@ -0,0 +1,19 @@
+package com.study.git;
+/**
+ * 
+ */
+
+/*
+ * 
+ */
+public class HelloWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello this is git!");
+	}
+
+}
