f1a545b725df7222e9731d8db3247b8b4d9a51bf
yzjd <xgg232@126.com>
2017-04-20 15:37:44 +0800

    测试分支 删除注释

--- GitStudy/src/com/study/git/HelloWord.java
+++ GitStudy/src/com/study/git/HelloWord.java
@@ -12,14 +12,7 @@ public class HelloWord {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//clone 分支 1
-		// 测试分支冲突
-		// 测试分支 1
-		// 测试分支 2
-		//clone 分支 1
-		// 测试分支 1
-		//clone 分支 2
+		
 		System.out.println("hello this is git!");
 	}
 
