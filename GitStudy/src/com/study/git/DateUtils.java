ac637fdaba306da995845908e8c4d5a0c8e1e768
yzjd <xgg232@126.com>
2017-06-23 22:03:01 +0800

    string date util

---
+++ GitStudy/src/com/study/git/DateUtils.java
@@ -0,0 +1,10 @@
+package com.study.git;
+
+public class DateUtils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("this is date tool");
+	}
+
+}
---
+++ GitStudy/src/com/study/git/StringUtil.java
@@ -0,0 +1,11 @@
+package com.study.git;
+
+public class StringUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is string util.");
+	}
+
+}
