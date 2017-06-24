c06695e4b01b2c38371dfa2da0535f00526b0ada
yzjd <xgg232@126.com>
2017-06-24 12:49:55 +0800

    master print 03

--- GitStudy/src/com/study/git/DateUtils.java
+++ GitStudy/src/com/study/git/DateUtils.java
@@ -9,6 +9,7 @@ public class DateUtils {
 		System.out.println("0623 print .");
 		System.out.println("gittest 02 master print 01");
 		System.out.println("master print 02");
+		System.out.println("master print 03");
 	}
 
 }
