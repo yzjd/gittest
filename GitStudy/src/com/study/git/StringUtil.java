cf7e119f1f669f1a7e2dddc00d0b211344430ad6
yzjd <xgg232@126.com>
2017-06-24 15:23:28 +0800

    0623 添加判断数字的方法

--- GitStudy/src/com/study/git/StringUtil.java
+++ GitStudy/src/com/study/git/StringUtil.java
@@ -1,6 +1,8 @@
 package com.study.git;
 
 public class StringUtil {
+	
+	private static String num = "0123456789";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,6 +10,22 @@ public class StringUtil {
 		System.out.println("this is string util.");
 		System.out.println("master print 01");
 	    System.out.println("0623 print 01.");
+	}
+	
+	/**
+	 * ÅÐ¶ÏÊÇ·ñÊý×Ö
+	 * @param s
+	 * @return
+	 */
+	public static boolean isNum(String s){
+		int size = s.length();
+		char c;
+		for(int i=0; i<size; i++){
+			c = s.charAt(i);
+			if(!num.contains(String.valueOf(c)))
+				return false;
+		}
+		return true;
 	}
 
 }
