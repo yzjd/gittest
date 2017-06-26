66a4d37f54bd8a9e6f86f5c5c58d69f96b54a0c4
yzjd <xgg232@126.com>
2017-06-26 22:15:30 +0800

    0620 print 03

--- GitStudy/src/com/study/git/HelloWord.java
+++ GitStudy/src/com/study/git/HelloWord.java
@@ -18,6 +18,7 @@ public class HelloWord {
 		System.out.println("0623 print 01 ");
 		System.out.println("0623 print 02 ");
 		System.out.println("0623 print 02 ");
+		System.out.println("0623 print 03 ");
 	}
 
 }
